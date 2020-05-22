952ac9ccea55a528fdca1caf0e36cca0af38ee21
Rojin Abraham <rojinabraham@gmail.com>
2020-05-22 13:37:03 +0530

    fin

--- script.js
+++ script.js
@@ -69,21 +69,47 @@ function createBox(item) {
   <p class="info">${text}</p> 
   `;
   //speak event
+  box.addEventListener("click", () => {
+    setTextMessage(text);
+    speakText();
 
+    //add active effect
+    box.classList.add("active");
+    setTimeout(() => box.classList.remove("active"), 1000);
+  });
   main.appendChild(box);
 }
+
+//innit speech synthesis
+const message = new SpeechSynthesisUtterance();
+
 //array to store voices
 
 let voices = [];
 function getVoices() {
   voices = speechSynthesis.getVoices();
   voices.forEach((voice) => {
-    console.log(voice);
+    // console.log(voice);
     const option = document.createElement("option");
     option.value = voice.name;
     option.innerText = `${voice.name} ${voice.lang}`;
     voiceSelect.appendChild(option);
   });
+}
+
+//set text
+function setTextMessage(text) {
+  message.text = text;
+}
+//speek text
+function speakText() {
+  speechSynthesis.speak(message);
+}
+//set voice
+function setVoice(e) {
+  console.log(e.target.value);
+  message.voice = voices.find((voice) => voice.name === e.target.value);
+  console.log(message.voice);
 }
 //voices changed
 speechSynthesis.addEventListener("voiceschanged", getVoices);
@@ -95,4 +121,12 @@ toggle.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   document.getElementById("text-box").classList.remove("show");
 });
+
+voiceSelect.addEventListener("change", setVoice);
+
+readBtn.addEventListener("click", () => {
+  setTextMessage(textArea.value);
+  speakText();
+});
+
 getVoices();
